2f3d80111a352d74b8680721f10b3e2f5bbb1135
Jan Řežab <honza@rezab.eu>
2023-11-13 21:55:51 +0100

    Write-LTLog initial version

--- LoggingTools/public/Set-LTLogDefaultLevel.ps1
+++ LoggingTools/public/Set-LTLogDefaultLevel.ps1
@@ -1,20 +1,22 @@
-<#
-.SYNOPSIS
-  Sets a global logging severity level
-.DESCRIPTION
-  This function sets a global logging severity level.
-  This level is used by all logging functions that do not have a -Level parameter.
-.PARAMETER Level
-  The severity level to set. Valid values are: DEBUG, INFO, WARNING, ERROR, CRITICAL
-.EXAMPLE
-  PS C:\> Set-LTLogDefaultLevel -Level INFO
-  PS C:\> Write-LTLog -Message "This is an INFO message"
-.LINK
-  https://github.com/rezabj/LoggingTools/blob/Dev/Docs/Set-LTLogDefaultLevel.md
-#>
-
 function Set-LTLogDefaultLevel {
-  [CmdletBinding(HelpUri='https://github.com/rezabj/LoggingTools/blob/Dev/Docs/Set-LTLogDefaultLevel.md')]
+  <#
+    .SYNOPSIS
+      Sets a global logging severity level
+    .DESCRIPTION
+      This function sets a global logging severity level.
+      This level is used by all logging functions that do not have a -Level parameter.
+    .PARAMETER Level
+      The severity level to set. Valid values are: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    .EXAMPLE
+      PS C:\> Set-LTLogDefaultLevel -Level INFO
+      PS C:\> Write-LTLog -Message "This is an INFO message"
+    .LINK
+      https://rezabj.github.io/LoggingTools/Functions/Set-LTLogDefaultLevel/
+    .LINK
+      https://github.com/rezabj/LoggingTools/blob/Dev/docs/Functions/Set-LTLogDefaultLevel.md
+  #>
+
+  [CmdletBinding(HelpUri='https://github.com/rezabj/LoggingTools/blob/Dev/docs/Functions/Set-LTLogDefaultLevel.md')]
   param (
     [Parameter(Mandatory=$true)]
     [ValidateSet('DEBUG','INFO','WARNING','ERROR','CRITICAL')]
---
+++ LoggingTools/public/Write-LTLog.ps1
@@ -0,0 +1,49 @@
+function Write-LTLog {
+  <#
+    .SYNOPSIS
+      Writes a log message to the target
+    .DESCRIPTION
+      This function writes a log message to the target.
+      The target can be a file, the console, or a custom target.
+      The default target is the console.
+    .PARAMETER Level
+      The severity level of the message. Valid values are: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    .PARAMETER Message
+      The message to write to the target
+    .PARAMETER ErrorRecord
+      An error record object to write to the target
+    .LINK
+      https://rezabj.github.io/LoggingTools/Functions/Write-LTLog/
+    .LINK
+      https://github.com/rezabj/LoggingTools/blob/Dev/docs/Functions/Write-LTLog.md
+  #>
+
+  [CmdletBinding(HelpUri='https://github.com/rezabj/LoggingTools/blob/Dev/docs/Functions/Write-LTLog.md')]
+  param (
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('DEBUG','INFO','WARNING','ERROR','CRITICAL')]
+    [string]$Level,
+    [Parameter(Mandatory=$true)]
+    [string]$Message,
+    [Parameter(Mandatory=$false)]
+    [System.Management.Automation.ErrorRecord]$ErrorRecord
+  )
+
+  if (($null -eq $Level) -and ($null -ne $Script:LoggingTools.Level)) {
+    $Level = $Script:LoggingTools.Level
+  } else {
+    $Level = 'INFO'
+  }
+
+  if (!$Script:LTLogQueue) {
+    $Script:LTLogQueue = New-Object System.Collections.Queue
+  }
+
+  $Script:LTLogQueue.Enqueue([PSCustomObject]@{
+    TimeStamp    = [datetime]::now
+    TimeStamputc = [datetime]::UtcNow
+    Level = $Level
+    Message = $Message
+    ErrorRecord = $ErrorRecord
+  })
+}
